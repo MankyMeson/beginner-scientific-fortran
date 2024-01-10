9ffafe64f39040f898e15b9e2dea30d603bdf365
Clio <cliokj22@gmail.com>
2024-01-10 10:50:08 +0000

    Added header for inclusion when compiling using pandoc

--- makeslides.sh
+++ makeslides.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 markdown_to_beamer () {
     pandoc $1 \
+        --include-in-header "$SCRIPT_DIR/header.tex"
         -t beamer \
         --pdf-engine=xelatex \
         -o $2
